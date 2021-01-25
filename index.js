a109659fc65778c14f12764f85bd831d81b3269a
NICKZZIN <76818604+NICKZZIN@users.noreply.github.com>
2021-01-24 23:58:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const nsfw = JSON.parse(fs.readFileSync('./src/nsfw.json'))
 const samih = JSON.parse(fs.readFileSync('./src/simi.json'))
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
+            + 'FN:Affis Admin\n'
             + 'ORG: Pengembang XBot;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=5511973027044:+55 11 97302-7044\n' 
             + 'END:VCARD' 
